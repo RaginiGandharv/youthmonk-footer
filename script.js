fe34d567805d9c847a2e99d0bef57b05ee32c812
RaginiGandharv <68639669+RaginiGandharv@users.noreply.github.com>
2021-07-14 11:57:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+    const drop_btn1 = document.querySelector(".mainmenu1");
+    const menu_toggle1 = document.querySelector(".submenu1");
+    drop_btn1.onclick = (() =>{
+      menu_toggle1.classList.toggle("show1");
+    });
+    const drop_btn2 = document.querySelector(".mainmenu2");
+    const menu_toggle2 = document.querySelector(".submenu2");
+    drop_btn2.onclick = (() => {
+      menu_toggle2.classList.toggle("show2");
+    });
+    const drop_btn3 = document.querySelector(".mainmenu3");
+    const menu_toggle3 = document.querySelector(".submenu3");
+    drop_btn3.onclick = (() => {
+      menu_toggle3.classList.toggle("show3");
+    });
+    const drop_btn4 = document.querySelector(".mainmenu4");
+    const menu_toggle4 = document.querySelector(".submenu4");
+    drop_btn4.onclick = (() => {
+      menu_toggle4.classList.toggle("show4");
+    });
